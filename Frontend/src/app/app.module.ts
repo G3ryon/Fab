514b399122377d955f6a251fba2825c1aa05fee0
Theo.ducoul <Dtheo98@gmail.com>
2019-11-17 16:00:45 +0100

    Revert "Beginning Angular"
    This reverts commit ca069efb7792554e983afabed744501950056be1.

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,45 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyComponentComponent } from './my-component/my-component.component';
-import { Impression3DComponent } from './impression3-d/impression3-d.component';
-import { AuthComponent } from './auth/auth.component';
-import { Routes,RouterModule } from '@angular/router';
-import { GalerieComponent } from './galerie/galerie.component';
-import { AcceuilComponent } from './acceuil/acceuil.component';
-import { FormationsComponent } from './formations/formations.component';
-import { EquipeComponent } from './equipe/equipe.component';
-import { ContactComponent } from './contact/contact.component';
-import { CalendrierComponent } from './calendrier/calendrier.component';
-import { ActivitesComponent } from './activites/activites.component';
 
-const appRoutes: Routes = [
-  {path: 'Impression3D', component: Impression3DComponent},
-  {path: '', component: AcceuilComponent},
-  {path: 'Galerie', component: GalerieComponent},
-  {path: 'Activites', component: ActivitesComponent},
-  {path: 'Calendrier', component: CalendrierComponent},
-  {path: 'Contact', component: ContactComponent},
-  {path: 'Equipe', component: EquipeComponent},
-  {path: 'Formations', component: FormationsComponent}
-];
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponentComponent,
-    Impression3DComponent,
-    AuthComponent,
-    GalerieComponent,
-    AcceuilComponent,
-    FormationsComponent,
-    EquipeComponent,
-    ContactComponent,
-    CalendrierComponent,
-    ActivitesComponent
+    MyComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
